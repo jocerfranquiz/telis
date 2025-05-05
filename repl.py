b4c7d62293416bfd384e76a84e46b377f52c3493
Jocer Franquiz <jocerfranquiz@gmail.com>
2025-05-04 22:42:07 -0500

    repl

--- repl.py
+++ repl.py
@@ -3,7 +3,7 @@
 import code
 import os
 import readline
-
+import sys
 
 REPL_HISTORY_FILE = os.path.expanduser(".repl_history")
 REPL_HISTORY_SIZE = 1024
@@ -13,13 +13,16 @@ if readline and os.path.exists(REPL_HISTORY_FILE):
 
 class Repl(code.InteractiveConsole):
   def runsource(self, source, filename='<input>', symbol='single'):
-    if not source.endswith(';'):
+    if not source.endswith('\n'):
       return True
-    print('source:', source)
-
-repl = Repl()
-repl.interact(banner='', exitmsg='')
+    # print('source:', source)
+    eval(source)
 
 if readline:
   readline.set_history_length(REPL_HISTORY_SIZE)
   readline.write_history_file(REPL_HISTORY_FILE)
+
+sys.ps1 = '[ '
+sys.ps2 = '  '
+repl = Repl()
+repl.interact(banner='', exitmsg='')
